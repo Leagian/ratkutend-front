5ab155e9fb70cbe052e9a60a5d8e5da25e4e4957
Alexandre Essaifi <alex-es-31@outlook.com>
2023-02-02 23:16:29 +0100

    blabla

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 
-=======
 import Parallax from "../components/Parallax";
 import Header from "../components/Header";
->>>>>>> 001header
 import Title from "../components/Title";
 
 const Home = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <Title />
-=======
     <div className="images-container home-global">
       <div className="home-header">
         <Header />
@@ -22,7 +15,6 @@ const Home = () => {
       <div className="home-title">
         <Title />
       </div>
->>>>>>> 001header
     </div>
   );
 };
