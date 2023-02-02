8bb7ce3cb6df2b55eea2766d2aeeb1f9f609b9ae
LEMMER JORDANE <jordane.lemmer@gmail.com>
2023-02-02 18:13:53 +0100

    first commit home PARALLAX

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       <div className="App">
-        <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </>
   );
