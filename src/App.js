6da6dc192532c89fcd27435d076d6c7b00bd8391
JeanLoic GARCIA <(mailto:garcia.jeanloic@gmail.com)>
2023-02-03 12:36:39 +0100

    map v1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Catalogue from "./screen/Catalogue";
 import Home from "./screen/Home";
 import Panier from "./screen/Panier";
-
+import Map from "./components/Map";
 function App() {
   return (
     <>
@@ -10,8 +10,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/panier" element={<Panier />} />
-
           <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/map" element={<Map />} />
         </Routes>
       </div>
     </>
